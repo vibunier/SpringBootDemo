7e4082cd67874548efcfbda792afd76c8d5b71ae
zhenyu <vibunier@gmail.com>
2018-07-27 21:56:19 +0800

    git init

---
+++ src/test/java/com/leetcode/solution/SolutionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leetcode.solution;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolutionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
